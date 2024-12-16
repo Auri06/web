f390ea27c82ecee66e745cd46dfe3d615815eeed
Auri06 <118132700+Auri06@users.noreply.github.com>
2024-12-16 19:17:00 -0400

    Create authors.js

---
+++ data/authors.js
@@ -0,0 +1,152 @@
+[
+  {
+    id: 1,
+    name: "George Orwell",
+    nationality: "British"
+  },
+  {
+    id: 2,
+    name: "Gabriel García Márquez",
+    nationality: "Colombian"
+  },
+  {
+    id: 3,
+    name: "Haruki Murakami",
+    nationality: "Japanese"
+  },
+  {
+    id: 4,
+    name: "Virginia Woolf",
+    nationality: "British"
+  },
+  {
+    id: 5,
+    name: "Pablo Neruda",
+    nationality: "Chilean"
+  },
+  {
+    id: 6,
+    name: "Leo Tolstoy",
+    nationality: "Russian"
+  },
+  {
+    id: 7,
+    name: "Margaret Atwood",
+    nationality: "Canadian"
+  },
+  {
+    id: 8,
+    name: "Jorge Luis Borges",
+    nationality: "Argentinian"
+  },
+  {
+    id: 9,
+    name: "Franz Kafka",
+    nationality: "Czech"
+  },
+  {
+    id: 10,
+    name: "Isabel Allende",
+    nationality: "Chilean"
+  },
+  {
+    id: 11,
+    name: "Chinua Achebe",
+    nationality: "Nigerian"
+  },
+  {
+    id: 12,
+    name: "Milan Kundera",
+    nationality: "Czech"
+  },
+  {
+    id: 13,
+    name: "Alice Walker",
+    nationality: "American"
+  },
+  {
+    id: 14,
+    name: "Julio Cortázar",
+    nationality: "Argentinian"
+  },
+  {
+    id: 15,
+    name: "Salman Rushdie",
+    nationality: "British-Indian"
+  },
+  {
+    id: 16,
+    name: "Toni Morrison",
+    nationality: "American"
+  },
+  {
+    id: 17,
+    name: "Mario Vargas Llosa",
+    nationality: "Peruvian"
+  },
+  {
+    id: 18,
+    name: "Kazuo Ishiguro",
+    nationality: "British-Japanese"
+  },
+  {
+    id: 19,
+    name: "Chimamanda Ngozi Adichie",
+    nationality: "Nigerian"
+  },
+  {
+    id: 20,
+    name: "Milan Kundera",
+    nationality: "Czech"
+  },
+  {
+    id: 21,
+    name: "José Saramago",
+    nationality: "Portuguese"
+  },
+  {
+    id: 22,
+    name: "Doris Lessing",
+    nationality: "British-Zimbabwean"
+  },
+  {
+    id: 23,
+    name: "Octavio Paz",
+    nationality: "Mexican"
+  },
+  {
+    id: 24,
+    name: "Italo Calvino",
+    nationality: "Italian"
+  },
+  {
+    id: 25,
+    name: "Nadine Gordimer",
+    nationality: "South African"
+  },
+  {
+    id: 26,
+    name: "Derek Walcott",
+    nationality: "Saint Lucian"
+  },
+  {
+    id: 27,
+    name: "Zadie Smith",
+    nationality: "British"
+  },
+  {
+    id: 28,
+    name: "Roberto Bolaño",
+    nationality: "Chilean"
+  },
+  {
+    id: 29,
+    name: "Orhan Pamuk",
+    nationality: "Turkish"
+  },
+  {
+    id: 30,
+    name: "J.M. Coetzee",
+    nationality: "South African"
+  }
+]
